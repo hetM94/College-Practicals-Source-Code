d836c454f6305030d1457a93636df6b6e4b92fcf
Het <149388359+hetM94@users.noreply.github.com>
2024-02-27 15:04:07 +0530

    Prac10 java

---
+++ Sem 4/AOOP/Prac8/Prac8.java
@@ -0,0 +1,18 @@
+class Demo{
+    int a =10;
+
+    Demo(int a)
+    {
+        this.a = a;
+    }
+
+    Demo()
+    {
+        a = a;
+    }
+
+    void show()
+    {
+        System.out.print("Value of a: "+ a);
+    }
+}
